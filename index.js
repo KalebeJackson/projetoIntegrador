5a6c309bc455bc589d1408b0b898723fa73a024b
Kalebe Jackson <santos.kalebe1234@gmail.com>
2021-11-22 12:58:30 -0300

    git pull

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({extended: false}))
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(loginRouter)
+app.use('../controllers/login')
 app.use(perfilRouter)
 
 
