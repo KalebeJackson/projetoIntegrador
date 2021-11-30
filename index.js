adf7970ee5245cc12c8def749452e067ebd394ac
Kalebe Jackson <santos.kalebe1234@gmail.com>
2021-11-30 19:55:53 -0300

    adicionado login controller e usuarios controller

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const app = express()
 const loginRouter = require('./routes/login')
 const perfilRouter = require('./routes/perfil')
 
-
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -17,14 +15,6 @@ app.use(perfilRouter)
 app.get('/',(req,res)=> {
 return res.render('home')
 })
-
-app.get('/login',(req,res)=> {
-    return res.render('login.html')
-    })
-
-app.get('/perfil',(req,res)=> {
-     return res.render('perfil')
-    })
 
 app.get('/cadastro',(req,res)=> {
     return res.render('cadastro')
--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const app = express()
 const router = express.Router()
 const loginController = require('../controlers/loginController')
 
 router.get('/login', loginController.index)
-
+app.get('/login',(req,res)=> {
+    return res.render('login')
+    })
+    
 module.exports = router;
