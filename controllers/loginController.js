1a5581407d04b34f7f5e4f0c58d3dd9f0804d9b7
Kalebe Jackson <santos.kalebe1234@gmail.com>
2021-12-14 20:44:59 -0300

    subindo atualizações do login Controller

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,6 +1,21 @@
 const loginController = {
     index: (req,res) =>{
         return res.render('login')
+    },
+    postLogin: (req, res) => {
+        Usuario
+        const { email, senha } = req.body
+
+        const usuario = Usuario.find(usuario => usuario.email == email)
+
+        if (!usuario || !bcrypt.compareSync(senha, usuario.senha)) {
+            return res.status(400).json({ mensagem: "Email ou senha estão incorretos ou não existem!" });
+        }
+
+
+        req.session.usuario = usuario
+
+        res.redirect('/servicos')
     }
 }
 
