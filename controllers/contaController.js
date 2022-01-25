1360b21bfc7fa88d6d51d73d1e19a19cff708da5
Kalebe Jackson <santos.kalebe1234@gmail.com>
2022-01-25 15:09:58 -0300

    atualizando contaController

--- controllers/contaController.js
+++ controllers/contaController.js
@@ -1,13 +1,15 @@
-const {Midia} = require('./../models')
+const { Midia } = require("./../models");
 
 const midiaController = {
-    index: async (req, res) => {
-        try {
-        const listar =  await Midia.findAll()
-        return res.json(listar)
-        } catch (error) {
-            console.log(error)
-        }
-    }}
+  index: async (req, res) => {
+    try {
+      const listar = await Midia.findAll();
+      // return res.render('../views/perfil')
+      return res.json(listar);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
 
-    module.exports =  midiaController
+module.exports = midiaController;
